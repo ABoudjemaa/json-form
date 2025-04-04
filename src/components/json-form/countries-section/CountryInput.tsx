a363ed373f87fda93830617778779c2464033d07
ABoudjemaa <boudjemaa.amine.2003@gmail.com>
2025-04-04 12:15:16 +0200

    fix responsive bug

--- src/components/json-form/countries-section/CountryInput.tsx
+++ src/components/json-form/countries-section/CountryInput.tsx
@@ -18,7 +18,7 @@ const CountryInput = ({
   <div className="flex flex-wrap gap-4 mb-4 px-2">
     <select
       disabled={!enabled}
-      className="flex-1 min-w-[200px] p-2 border border-gray-300 rounded-md shadow-sm "
+      className="w-1/3 md:flex-1 md:min-w-[200px] p-2 border border-gray-300 rounded-md shadow-sm "
       value={newCountry}
       onChange={(e) => onCountryChange(e.target.value)}
     >
@@ -34,7 +34,7 @@ const CountryInput = ({
       min="0"
       max="100"
       step="0.1"
-      className="w-24 p-2 border border-gray-300 rounded-md shadow-sm "
+      className="w-20 md:w-24 p-2 border border-gray-300 rounded-md shadow-sm "
       placeholder="%"
       disabled={!enabled}
       value={newPercent}
@@ -43,7 +43,7 @@ const CountryInput = ({
     <button
       type="button"
       onClick={onAdd}
-      className={`${!enabled ? "":" hover:bg-gray-800 "}` +"w-1/6 px-4 py-2 bg-black text-white rounded-md  focus:outline-none focus:ring-2"}
+      className={`${!enabled ? "":" hover:bg-gray-800 "}` +"w-20 md:w-1/6 px-4 py-2 bg-black text-white rounded-md  focus:outline-none focus:ring-2"}
     >
       Add
     </button>
--- src/components/json-form/countries-section/CountryItemDisplay.tsx
+++ src/components/json-form/countries-section/CountryItemDisplay.tsx
@@ -13,13 +13,13 @@ export const CountryItemDisplay = ({
   onRemove: (index: number) => void;
 }) => (
   <div className="p-3 flex flex-wrap gap-4 mb-4 items-center justify-between">
-    <div className="flex-1 font-medium">{item.country}</div>
-    <div className="w-24 p-2 border border-gray-300 rounded-md shadow-sm ">{item.percent || ''}</div>
+    <div className="w-1/3 md:flex-1 font-medium">{item.country}</div>
+    <div className="w-20 md:w-24 p-2 border border-gray-300 rounded-md shadow-sm ">{item.percent || ''}</div>
     <button
       disabled={!enabled}
       type="button"
       onClick={() => onRemove(index)}
-      className={`${!enabled ? "" : " hover:bg-red-800 "}` + "w-1/6 px-4 py-2 bg-red-600  text-white rounded-md flex items-center justify-center"}
+      className={`${!enabled ? "" : " hover:bg-red-800 "}` + "w-18 md:w-1/6 px-4 py-2 bg-red-600  text-white rounded-md flex items-center justify-center"}
     >
       Delete
     </button>
